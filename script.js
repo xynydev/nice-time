f360bfc9d50dab099133f13a30da417854b1e3f2
xynydev <rauhalaeino@gmail.com>
2024-08-13 21:07:11 +0300

    fix: pad numbers

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function clock() {
   const time = document.getElementById('time');
 
   const now = new Date();
-  let h = now.getHours();
-  let m = now.getMinutes();
+  let h = now.getHours().toString();
+  let m = now.getMinutes().toString();
+  if (h.length < 2) h = '0'+h
+  if (m.length < 2) m = '0'+m
 
   if (7 < h < 22) {
     time.className = 'filled';
